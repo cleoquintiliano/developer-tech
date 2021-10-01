8a9d1ac716e4e94502b05d7f9d69ca51ff049edc
unknown <you@example.com>
2021-10-01 08:29:30 -0300

    Simulador de Financiamento

---
+++ Mod_1/simulador-financiamento/simulador.js
@@ -0,0 +1,41 @@
+function simular() {
+  var valor = document.getElementById("valor").valueAsNumber;
+  var prazo = document.getElementById("prazo").valueAsNumber;
+  var jurosaa = document.getElementById("jurosaa").valueAsNumber;
+  var prestacoes = prazo * 12;
+  //var jurosam = (1 + jurosaa) ** (1 / 12) - 1;
+  var jurosam = Math.pow(1 + jurosaa, 1 / 12) - 1;
+  var amortizacao = valor / prestacoes;
+
+  document.getElementById("prestacoes").valueAsNumber = prestacoes;
+  document.getElementById("jurosam").valueAsNumber = jurosam;
+
+  var tbody = document.querySelector("tbody");
+  var jurosTotal = 0;
+  for (var i = 0; i < prestacoes; i++) {
+    var saldoDevedor = valor - i * amortizacao;
+    var jurosPrestacao = saldoDevedor * jurosam;
+    jurosTotal += jurosPrestacao;
+
+    if (i < 5) {
+      var totalPrestacao = jurosPrestacao + amortizacao;
+      var tr = tbody.children[i];
+      tr.children[1].textContent = amortizacao.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+      tr.children[2].textContent = jurosPrestacao.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+      tr.children[3].textContent = totalPrestacao.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    }
+  }
+
+  document.getElementById("jurostotal").valueAsNumber = jurosTotal.toFixed(2);
+}
+
+simular();
